78809aa804e493bec01446bcae852341483ed315
Dan Park <dpark.sports@gmail.com>
2020-09-15 15:59:35 -0700

    adds build

--- projection_matrix.cpp
+++ projection_matrix.cpp
@@ -1,37 +1,18 @@
-#pragma once
+#include <Eigen/Core>
+#include <Eigen/Cholesky>
+#include <Eigen/Dense>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/eigen.hpp>
 #include <iostream>
 #include <chrono>
-#include <Eigen/Core>
-#include <Eigen/Cholesky>
-#include <Eigen/Dense>
 
 using namespace std;
 using namespace std::chrono;
 using namespace Eigen;
 using namespace std;
 
-void compute_projection_matrix_SVD() {
-    Eigen::MatrixXf pts2d(6,2), pts3d(6,3);
-    pts3d <<
-          1.5706,-0.1490,0.2598,
-            -1.5282, 0.9695, 0.3802,
-            -0.6821, 1.2856, 0.4078,
-            0.4124, -1.0201, -0.0915,
-            1.2095, 0.2812, -0.1280,
-            0.8819, -0.8481, 0.5255;
-
-    pts2d <<
-          1.0486, -0.3645,
-            -1.6851, -0.4004,
-            -0.9437, -0.4200,
-            1.0682, 0.0699,
-            0.6077, -0.0771,
-            1.2543, -0.6454;
-
-
+cv::Mat compute_projection_matrix_SVD(Eigen::MatrixXf pts2d, Eigen::MatrixXf pts3d) {
     assert(pts2d.rows() == pts3d.rows() && pts2d.cols() == 2 && pts3d.cols() == 3);
 
     const size_t rows = pts3d.rows() * 2;
@@ -56,41 +37,18 @@ void compute_projection_matrix_SVD() {
     cv::Mat cvSol;
     cv::eigen2cv(smallest, cvSol);
     cv::Mat projectionMatrixSVD = cvSol.reshape(0, 3);
+
+    return projectionMatrixSVD;
 }
 
-void compute_projection_matrix_LS() {
-    Eigen::MatrixXf pts2d(6,2), pts3d(6,3);
-    pts3d <<
-          1.5706,-0.1490,0.2598,
-            -1.5282, 0.9695, 0.3802,
-            -0.6821, 1.2856, 0.4078,
-            0.4124, -1.0201, -0.0915,
-            1.2095, 0.2812, -0.1280,
-            0.8819, -0.8481, 0.5255;
-
-    pts2d <<
-          1.0486, -0.3645,
-            -1.6851, -0.4004,
-            -0.9437, -0.4200,
-            1.0682, 0.0699,
-            0.6077, -0.0771,
-            1.2543, -0.6454;
-
-
-    std::cout <<pts3d.rows()  << "," << pts3d.cols() << std::endl;
-    std::cout <<pts2d.rows()  << "," << pts2d.cols() << std::endl;
-
-    std::cout << pts3d << endl;
-    std::cout << pts2d << endl;
-
+cv::Mat compute_projection_matrix_LS(Eigen::MatrixXf pts2d, Eigen::MatrixXf pts3d) {
     assert(pts2d.rows() == pts3d.rows() && pts2d.cols() == 2 && pts3d.cols() == 3);
-    // Set up A and b matrices.
+
     const size_t rows = pts3d.rows() * 2;
     size_t cols = 11;
     Eigen::MatrixXf A(rows, cols);
     Eigen::MatrixXf b(rows, 1);
 
-    // Build A and b matrices
     for (int i = 0; i < rows; i += 2) {
         float X = pts3d(i / 2, 0);
         float Y = pts3d(i / 2, 1);
@@ -105,7 +63,6 @@ void compute_projection_matrix_LS() {
 
     cols = 12;
 
-    // Solve least squares
     Eigen::MatrixXf leastsquare = (A.transpose() * A).ldlt().solve(A.transpose() * b);
     leastsquare.conservativeResize(leastsquare.rows() + 1, leastsquare.cols());
     leastsquare(leastsquare.rows() - 1, 0) = 1;
@@ -115,10 +72,11 @@ void compute_projection_matrix_LS() {
     cv::Mat projectionMatrix = leaseSquareCV.reshape(0, 3);
     std::cout << "projectionMatrix:" << projectionMatrix << endl;
     std::cout << projectionMatrix.size()  << endl;
+
+    return projectionMatrix;
 }
 
 void compute_camera_center(cv::Mat projectionMatrix) {
-        // Find center of camera
     cv::Mat Q = projectionMatrix.colRange(0, 3);
     std::cout << "Q:" << Q << endl;
     std::cout << Q.size()  << endl;
@@ -137,7 +95,7 @@ void compute_camera_center(cv::Mat projectionMatrix) {
     std::cout << "cameraCenter:" << cameraCenter << endl;
 }
 
-void estimate_residual() {
+void estimate_residual(Eigen::MatrixXf pts2d, Eigen::MatrixXf pts3d, cv::Mat projectionMatrix) {
     cv::Mat cv3dpts, cv2dpts;
     cv::eigen2cv(pts3d, cv3dpts);
     cv::eigen2cv(pts2d, cv2dpts);
@@ -162,11 +120,33 @@ void estimate_residual() {
     double residual = cv::norm(projected2d, transposed2D);
 }
 
+void test_projection_matrix() {
+    Eigen::MatrixXf pts2d(6,2), pts3d(6,3);
+
+    pts3d <<
+          1.5706,-0.1490,0.2598,
+            -1.5282, 0.9695, 0.3802,
+            -0.6821, 1.2856, 0.4078,
+            0.4124, -1.0201, -0.0915,
+            1.2095, 0.2812, -0.1280,
+            0.8819, -0.8481, 0.5255;
+
+    pts2d <<
+          1.0486, -0.3645,
+            -1.6851, -0.4004,
+            -0.9437, -0.4200,
+            1.0682, 0.0699,
+            0.6077, -0.0771,
+            1.2543, -0.6454;
+
+    cv::Mat p_matrix_LS = compute_projection_matrix_LS(pts3d, pts2d);
+    cv::Mat p_matrix_SVD = compute_projection_matrix_SVD(pts3d, pts2d);
+    compute_camera_center(p_matrix_LS);
+    compute_camera_center(p_matrix_SVD);
+    estimate_residual(pts3d, pts2d, p_matrix_LS);
+    estimate_residual(pts3d, pts2d, p_matrix_SVD);
+}
 
 int main() {
-
-    compute_projection_matrix_LS();
-    compute_projection_matrix_SVd();
-    compute_camera_center();
-    estimate_residual();
+    test_projection_matrix();
 }
